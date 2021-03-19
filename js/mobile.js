c70b93a960ed25eb3aa1c0a1ebb8231a927cf2a7
FLIPIT-LAB <63399830+FLIPIT-LAB@users.noreply.github.com>
2021-03-19 11:39:49 +0000

    Add files via upload

---
+++ js/mobile.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+// Back too top#############
+var btn = $('#to-top');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
